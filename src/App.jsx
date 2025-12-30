1b58ca970b8d79a58c68641a02236eeaabab5dc6
Dishon. C <consalvesdishon16@gmail.com>
2025-12-30 11:37:02 +0530

    fixed subjects

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,8 @@ import StudentLogin from "./pages/student/StudentLogin.jsx";
 import StudentDashboard from "./pages/student/StudentDashboard.jsx";
 import StudentSection from "./pages/student/StudentSection.jsx";
 import StudentPersonalDetails from "./pages/student/StudentPersonalDetails.jsx";
+import StudentSubjectList from "./pages/student/StudentSubjectList.jsx";
+import StudentCertificate from "./pages/student/StudentCertificate.jsx";
 import Batches from "./pages/exam/Batches.jsx";
 import Courses from "./pages/exam/Courses.jsx";
 import Students from "./pages/exam/Students.jsx";
@@ -308,6 +310,22 @@ export default function App() {
           element={
             <GuardedRoute isAuthed={isStudentAuthed} redirectTo="/student/login">
               <StudentPersonalDetails />
+            </GuardedRoute>
+          }
+        />
+        <Route
+          path="/student/course-list"
+          element={
+            <GuardedRoute isAuthed={isStudentAuthed} redirectTo="/student/login">
+              <StudentSubjectList />
+            </GuardedRoute>
+          }
+        />
+        <Route
+          path="/student/certificate"
+          element={
+            <GuardedRoute isAuthed={isStudentAuthed} redirectTo="/student/login">
+              <StudentCertificate />
             </GuardedRoute>
           }
         />
