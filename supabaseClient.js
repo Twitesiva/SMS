bdef72984766e370fb9d5019340ffbe5e727d2ca
Pravinkumar003 <pravinkumar2002m@gmail.com>
2026-02-18 14:55:21 +0530

    update migrate

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // ✅ Replace these with your actual project values
-const supabaseUrl = 'https://mnhpvhvtihxtmlbmovpz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uaHB2aHZ0aWh4dG1sYm1vdnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxMzExMzUsImV4cCI6MjA4NDcwNzEzNX0.guiXVrebWm0S2G9Wb-KuP-n5QmmdZodzw8WenHZtsKw'
+const supabaseUrl = 'https://pedgdgqghjevxjawrksx.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlZGdkZ3FnaGpldnhqYXdya3N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzOTE5MTAsImV4cCI6MjA4Njk2NzkxMH0.Y1ZFgl3uM1-WoZ4Aiv8NuoQjungW_He7NGnHG4InsdE'
 
 // Initialize Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
